f17888f372e8b973dabd34f4df89ec843ec0b81f
Charlie Carter <charliec1665@gmail.com>
2022-08-20 17:02:08 -0400

    js pseudocode

---
+++ assets/js/script.js
@@ -0,0 +1,16 @@
+// get info from local storage, parse for button
+    // forEach call display
+
+// search form event listener
+
+// function to fetch OpenWeather api info
+    // fetch weather for current date according to city name entered in form
+    // fetch 5-day-forecast
+    // local storage set item, stringify for button
+    // call display function
+    // catch
+
+// function to display weather info
+    // get weather info column using jquery
+    // access weather data from the fetch
+    // append weather data to parent container
