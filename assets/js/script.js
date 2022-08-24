63faa26a7f7a47707c7a15f5a361e279923351c2
Charlie Carter <charliec1665@gmail.com>
2022-08-24 18:56:08 -0400

    append button function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,10 @@
 var weatherInfoEl = $('#weather-info');
-var forecastInfoEl = $('#forecast-info');
+var displayContainer = $('#display-column');
+var forecastInfoContainer = $('#forecast-info-container');
+var forecastCardContainer = $('#info-card-container');
 var searchForm = $('#search-city');
 var cityInput = $('#city-name');
+var cityButtonEl = $('city-btn');
 
 // get info from local storage
 var currentCityWeather = JSON.parse(localStorage.getItem('weather'));
@@ -28,6 +31,27 @@ searchForm.submit(function(event) {
     cityInput.val("");
 });
 
+// city button event listener
+cityButtonEl.submit(function(event) {
+    event.preventDefault();
+
+    var city = cityInput.val();
+    if (city) {
+        searchWeather(city);
+    }
+
+    cityInput.val("");
+})
+
+function timeConverter(unixTimestamp) {
+    var stamp = new Date(unixTimestamp * 1000);
+    var month = stamp.getMonth();
+    var date = stamp.getDate();
+    var year = stamp.getFullYear();
+    var time = date + '/' + month + '/' + year;
+    return time;
+}
+
 // function to fetch OpenWeather api info
 function searchWeather(cityWeather) {
     // cityWeather = cityWeather.toLowercase();
@@ -41,17 +65,38 @@ function searchWeather(cityWeather) {
             // grab lat and long from initial api info return
             var latitude = data.coord.lat;
             var longitude = data.coord.lon;
-            console.log(latitude);
-            console.log(longitude);
 
             // save current weather to local storage
             var currentWeather = JSON.parse(localStorage.getItem('weather'));
-            // if (!currentWeather)
-            //     currentWeather = [];
-            
-            // local storage set item, stringify for button
+            // if there is no weather stored then set to empty array
+            if (!currentWeather)
+                currentWeather = [];
+    
+            // if the weather for the searched city isnt already in storage loop through and grab
+            var alreadySaved = false;
+            console.log(currentWeather);
+            currentWeather.forEach(function (city) {
+                var name = city.name;
+                if (name === cityWeather) {
+                    alreadySaved = true;
+                    // create and append button!
+                    var cityBtn = $('<button class="btn btn-primary col-12 p-1 mt-3">').text(name);
+                    cityButtonEl.append(cityBtn);
+                }
+            });
+
+            if (!alreadySaved) {
+                // if we didnt find a match above add to city array
+                currentCityWeather.push({
+                    name: cityWeather,
+                    data: data
+                });
+            }
+
+            // local storage set item
             localStorage.setItem('weather', JSON.stringify(currentWeather));
 
+            // create header with city name
             var weatherHeaderEl = $('#weather-header');
             weatherHeaderEl.text('Current Weather: ' + data.name);
             weatherInfoEl.prepend(weatherHeaderEl);
@@ -67,8 +112,6 @@ function searchWeather(cityWeather) {
                 console.log(data);
                 // save 5 day forecast to local storage
                 var fivedayForecast = JSON.parse(localStorage.getItem('fiveday'));
-                // if (!fivedayForecast)
-                //     fivedayForecast = [];
                 
                 // local storage set item, stringify for button
                 localStorage.setItem('fiveday', JSON.stringify(fivedayForecast));
@@ -87,44 +130,67 @@ function searchWeather(cityWeather) {
 
 // function to display weather info
 function displayWeatherInfo(weatherData) {
+
+    weatherInfoEl.css({"margin" : "15px", "border" : "solid 1px rgb(254, 171, 108)"});
+
     var currentTempItem = $('#temp');
     var currentHumItem = $('#humidity');
     var currentWindItem = $('#wind');
     var currentUviItem = $('#uvi');
 
     var currentTemp = Math.round(((weatherData.current.temp - 273.15) * 9/5 + 32) * 10) / 10;
-    currentTempItem.text('Temp: ' + currentTemp);
+    currentTempItem.text('Temp: ' + currentTemp + 'F');
 
     var currentHumidity = weatherData.current.humidity;
-    currentHumItem.text('Humidity: ' + currentHumidity);
+    currentHumItem.text('Humidity: ' + currentHumidity + '%');
 
     var currentWind = weatherData.current.wind_speed;
-    currentWindItem.text('Wind: ' + currentWind);
+    currentWindItem.text('Wind: ' + currentWind + 'MPH');
 
     var currentUvi = weatherData.current.uvi;
     currentUviItem.text('UV Index: ' + currentUvi);
    
-    var forecastHeaderEl = $('#f-header');
-    forecastHeaderEl.text('5-Day Forecast');
-    forecastInfoEl.append(forecastHeaderEl);
+    var forecastHeaderEl = $('<h2 id="f-header">');
+    forecastHeaderEl.text('5-Day Forecast:');
+    forecastInfoContainer.prepend(forecastHeaderEl);
 
    for (i = 0; i < 5; i++) {
-        // create forecast daily card
-        
-        var forecastTempItem = $('#temp');
-        var forecastHumItem = $('#humidity');
-        var forecastWindItem = $('#wind');
 
         var forecastData = weatherData.daily[i];
 
+        // create forecast daily card
+        var forecastCard = $('<div id="f-card" class="border-0">');
+        // for each day create card title with date
+        var forecastTimeStamp = forecastData.dt;
+        var forecastDate = timeConverter(forecastTimeStamp);
+        // var forecastDateFormat = moment(forecastDate.val("")).format('DD/MM/YYYY');
+        var forecastTitle = $('<h3 class="m-3 text-light">').text(forecastDate);
+        // create card text with pulled info by appending li to ul
+        var forecastText = $('<ul class="m-3 p-1 text-light">');
+
+        // grab icon
+        var forecastIcon =  $('<img class="p-1">').attr("src", "http://openweathermap.org/img/wn/" + forecastData.weather[0].icon + "@2x.png");
+        forecastText.append(forecastIcon);
+
+        // grab and create temp li
         var forecastTemp = Math.round(((forecastData.temp.day - 273.15) * 9/5 + 32) * 10) / 10;
-        forecastTempItem.text('Temp: ' + forecastTemp);
+        var forecastTempItem = $('<li class="p-1">').text('Temp: ' + forecastTemp);
+        forecastText.append(forecastTempItem);
 
+        // grab and create wind li
         var forecastWind = forecastData.wind_speed;
-        forecastWindItem.text('Wind: ' + forecastWind);
+        var forecastWindItem = $('<li class="p-1">').text('Wind: ' + forecastWind);
+        forecastText.append(forecastWindItem);
 
+        // grab and create humidity li
         var forecastHum = forecastData.humidity;
-        forecastHumItem.text('Humidity: ' + forecastHum);
+        var forecastHumItem = $('<li class="p-1">').text('Humidity: ' + forecastHum);
+        forecastText.append(forecastHumItem);
+
+        // append card divs to forecast container
+        forecastCardContainer.append(forecastCard);
+        forecastCard.append(forecastTitle);
+        forecastCard.append(forecastText);
    }; 
 }
     
